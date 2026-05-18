4828b8958025e69720ab5f6a63397f5b58302125
Nat <furinaawlover3@gmail.com>
2026-05-18 12:41:28 +0700

    Add AxionOS flags and device configuration

--- lineage_a22.mk
+++ lineage_a22.mk
@@ -11,3 +11,12 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/a22/common_a22.mk)
 
 PRODUCT_NAME := lineage_a22
+
+# AxionOS flags
+TARGET_DISABLE_EPPE := true
+AXION_MAINTAINER := straawwhaatz
+AXION_PROCESSOR := Helio_G80
+AXION_CAMERA_REAR_INFO := 48,5,2
+AXION_CAMERA_FRONT_INFO := 13
+AXION_CPU_SMALL_CORES := 0,1,2,3
+AXION_CPU_BIG_CORES := 4,5,6,7
